68ae217c92985f698b2a47f1689ce7529dae80c6
rashel9454 <rashelrana931@gmail.com>
2022-04-26 16:59:17 +0600

    26/04/2022

---
+++ indext.php
@@ -0,0 +1,64 @@
+<h2>Question -1</h2>
+<?php
+/**Create a function that will return children, teenagers, young people, old people from the age */
+//Answer//
+
+
+
+
+?>
+<h2>Question -2</h2>
+<?php
+/**Create a function that will return the area of a rectangle, square, triangle
+
+ */
+//Answer//
+
+
+
+?>
+<h2>Question -3</h2>
+<?php
+/**GPA function for result publishing
+
+ */
+//Answer//
+
+
+
+
+?>
+<h2>Question -4</h2>
+<?php
+/**Create an age calculator function
+
+ */
+//Answer//
+
+
+
+
+?>
+<h2>Question -5</h2>
+<?php
+/**Create a BMI function for health
+
+ */
+//Answer//
+
+
+
+
+?>
+<h2>Question -6</h2>
+<?php
+/**Create a currency converter function from taka to USD, CAD, POUND, EURO etc
+
+
+ */
+//Answer//
+
+
+
+
+?>
